b1e564b47936c4c7ed597e931e116859beb3f90a
NaliSong <nalisong648@gmail.com>
2023-08-19 19:01:44 +0900

    ItemData 데이터 저장

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using UnityEngine;
 
@@ -18,13 +20,42 @@ public class ItemManager : Singleton<ItemManager>
 
         if (readResult == null)
         {
-            Debug.LogError("CSV Result is Null");
+            UnityEngine.Debug.LogError("CSV Result is Null");
             return;
         }
 
-        foreach (var data in readResult)
+        try
         {
-            Debug.Log($"Index: {data["Index"]}");
+            foreach (var data in readResult)
+            {
+                var newItemData = new ItemData();
+                newItemData.Index = (int)data["Index"];
+                newItemData.Location = (string)data["Location"];
+                if (Enum.TryParse((string)data["Type"], out newItemData.Type) == false)
+                {
+                    newItemData.Type = ItemData.ItemType.None;
+                }
+                newItemData.Name = (string)data["Name"];
+                if (Boolean.TryParse((string)data["IsAnswer"], out newItemData.IsAnswer) == false)
+                {
+                    newItemData.IsAnswer = false;
+                }
+                if (Boolean.TryParse((string)data["NeedConvert"], out newItemData.NeedConvert) == false)
+                {
+                    newItemData.NeedConvert = false;
+                }
+                newItemData.Answer = (string)data["Answer"];
+                newItemData.Description = (string)data["Description"];
+
+                if (ItemDatadic.ContainsKey(newItemData.Index) == false)
+                {
+                    ItemDatadic.Add(newItemData.Index, newItemData);
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            UnityEngine.Debug.LogError(e.Message);
         }
     }
 }
