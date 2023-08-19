26605cd2746a6b9a76701dd84998af57490f3b08
NaliSong <nalisong648@gmail.com>
2023-08-19 15:24:52 +0900

    InventoryComponent -> ItemComponent로 변환

--- Assets/Scripts/ItemConverterComponent.cs
+++ Assets/Scripts/ItemConverterComponent.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemConverterComponent : MonoBehaviour
 {
+    public ItemComponent ItemComponent;
+    public ScrollRect InputScrollRect;
+    public ScrollRect OutputScrollRect;
+    public Button ConvertButton;
+
+    private List<ItemComponent> _inputItemComponentList =new List<ItemComponent>();
+    private List<ItemComponent> _outputItemComponentList = new List<ItemComponent>();
+
     private ItemConverterData _itemConverterData;
 
     private void Awake()
@@ -18,6 +27,46 @@ public class ItemConverterComponent : MonoBehaviour
     public void SetItemConverterData(ItemConverterData itemConverterData)
     {
         _itemConverterData = itemConverterData;
+        ConvertButton.interactable = _itemConverterData.GetInputItemDataList().Count > 0;
+        SetInputItemComponent();
+    }
+
+    private void SetInputItemComponent()
+    {
+        if (_itemConverterData == null)
+        {
+            return;
+        }
+
+        var itemDataList = _itemConverterData.GetInputItemDataList();
+        if (itemDataList == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < itemDataList.Count; i++)
+        {
+            if (itemDataList[i] == null)
+            {
+                continue;
+            }
+
+            if (i >= _inputItemComponentList.Count)
+            {
+                var compo = Instantiate(ItemComponent, InputScrollRect.content);
+                if (compo == null)
+                {
+                    return;
+                }
+
+                _inputItemComponentList.Add(compo);
+            }
+
+            _inputItemComponentList[i].ItemCountText.text = itemDataList[i].Count.ToString();
+            _inputItemComponentList[i].ItemData = itemDataList[i];
+            _inputItemComponentList[i].NowItemType = ItemComponent.ItemType.ConverterInput;
+            _inputItemComponentList[i].gameObject.SetActive(true);
+        }
     }
 
     public void AddItem(ItemData itemData)
@@ -35,5 +84,14 @@ public class ItemConverterComponent : MonoBehaviour
         }
 
         _itemConverterData.AddItem(itemData);
+        ConvertButton.interactable = _itemConverterData.GetInputItemDataList().Count > 0;
+
+        SetInputItemComponent();
     }
+
+    public void OnClickConvertButton()
+    {
+
+    }
+
 }
