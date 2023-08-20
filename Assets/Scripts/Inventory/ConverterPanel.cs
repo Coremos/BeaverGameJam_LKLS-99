5a564497feea036a5788e2423852b0df83c799c7
NaliSong <nalisong648@gmail.com>
2023-08-20 09:47:34 +0900

    변환기 정보 표시

--- Assets/Scripts/Inventory/ConverterPanel.cs
+++ Assets/Scripts/Inventory/ConverterPanel.cs
@@ -6,28 +6,27 @@ using static UnityEditor.Progress;
 
 public class ConverterPanel : MonoBehaviour
 {
-    public List<ItemConverterData> ConverterDataList = new List<ItemConverterData>();
+    public List<ItemConverterComponent> ConverterComponentList = new List<ItemConverterComponent>();
     private void Awake()
     {
         SetConverter(GameDataManager.Instance.GlobalInventory.Items);
 
-        foreach (var converterData in ConverterDataList)
+        foreach (var converterComponent in ConverterComponentList)
         {
-            if (ItemManager.Instance.ConverterDic.ContainsKey(converterData.ConverterName))
+            if (ItemManager.Instance.ConverterDic.ContainsKey(converterComponent.ItemConverterData.ConverterName))
             {
-                var hasItem = ItemManager.Instance.ConverterDic[converterData.ConverterName];
-                converterData.gameObject.SetActive(hasItem);
+                var hasItem = ItemManager.Instance.ConverterDic[converterComponent.ItemConverterData.ConverterName];
+                converterComponent.gameObject.SetActive(hasItem);
             }
             else
             {
-                converterData.gameObject.SetActive(false);
+                converterComponent.gameObject.SetActive(false);
             }
         }
     }
 
     private void SetConverter(List<ItemData> itemDataList)
     {
-        var isNew = false;
         foreach (ItemData item in itemDataList)
         {
             if (item.Type == ItemData.ItemType.Converter)
@@ -35,14 +34,26 @@ public class ConverterPanel : MonoBehaviour
                 if (ItemManager.Instance.ConverterDic.ContainsKey(item.Name))
                 {
                     ItemManager.Instance.ConverterDic[item.Name] = true;
-                    isNew = true;
+                    SetInfoPane(item.Name, item.Description);
                 }
             }
         }
 
-        if (isNew)
+    }
+
+    private void SetInfoPane(string converterName, string discription)
+    {
+        foreach (var compo in  ConverterComponentList)
         {
-            // 팝업창
+            if (string.Compare(compo.ItemConverterData.ConverterName, converterName) != 0)
+            {
+                continue;
+            }
+
+            compo.InfoPanel.gameObject.SetActive(true);
+            compo.NameText.text = converterName;
+            compo.DiscriptionText.text = discription;
+            break;
         }
     }
 }
