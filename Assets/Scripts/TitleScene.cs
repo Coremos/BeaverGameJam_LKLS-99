08776234703e61f411b58979aab8b5baa898e728
Coremos <47705451+Coremos@users.noreply.github.com>
2023-08-24 23:55:38 +0900

    타이틀 씬 코드 수정
    - 로딩을 체크 씬에서 진행하도록 변경하여 해당 내용 삭제

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class TitleScene : MonoBehaviour
 {
-    [SerializeField] private ItemSpriteBinder spriteBinder;
-
     public void LoadCheckScene()
     {
         GameSceneManager.Instance.LoadScene(GameSceneManager.SceneType.Check);
-        GameDataManager.Instance.GlobalInventory.ClearItems();
-        GameDataManager.Instance.PlayerInventory.ClearItems();
-        ItemManager.Instance.Init();
-        SubmitManager.Instance.Init();
-        spriteBinder.Bind(ItemManager.Instance.ItemTabeDataDic);
     }
 }
