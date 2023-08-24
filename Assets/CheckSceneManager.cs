301158b3a80b50c01fc4228cbae2b9b5214c2238
Coremos <47705451+Coremos@users.noreply.github.com>
2023-08-24 23:54:04 +0900

    체크씬 매니저 수정
    - 매번 타이틀 씬에서 초기화를 한 다음 게임을 테스트하는 것이 번거로워 해당 씬에서 초기화하도록 수정

--- Assets/CheckSceneManager.cs
+++ Assets/CheckSceneManager.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class CheckSceneManager : MonoBehaviour
 {
+    [SerializeField] private ItemSpriteBinder spriteBinder;
     [SerializeField] private Timer timer;
     [SerializeField] private TitleAnimationController titleAnimationController;
 
-    private void Start()
+    private void Awake()
     {
+        GameDataManager.Instance.GlobalInventory.ClearItems();
+        GameDataManager.Instance.PlayerInventory.ClearItems();
+        ItemManager.Instance.Init();
+        SubmitManager.Instance.Init();
+        spriteBinder.Bind(ItemManager.Instance.ItemTabeDataDic);
+
         titleAnimationController.Add("기자들이 다가온다…!");
         titleAnimationController.Add("아직 초전도체는 미완성이야!");
         titleAnimationController.Add("재료를 모아 초전도체 지하연구실로 이동하자!");
